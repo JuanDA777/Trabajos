62615a1ccdd87867ce386c9bc4c76bb80afa1c16
Juan Diego <juand.arevalo221@umariana.edu.co>
2023-03-02 22:51:25 -0500

    Calculadora suma

---
+++ calculadora/src/controlador/Operaciones.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+/**
+ *
+ * @author juand
+ */
+public class Operaciones {
+    public String sumar(int numero1, int numero2){
+        int resultado = numero1 + numero2;
+        return resultado +"";
+    }
+    public String restar(int numero1, int numero2){
+        int resultado = numero1 - numero2;
+        return resultado +"";
+    }
+    public String multiplicar(int numero1, int numero2){
+        int resultado = numero1 * numero2;
+        return resultado +"";
+    }
+    public String dividir(int numero1, int numero2){
+        int resultado = numero1 / numero2;
+        return resultado +"";
+    }
+    
+}
