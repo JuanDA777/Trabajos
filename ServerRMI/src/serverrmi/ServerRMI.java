d2ec8c188b44681c2b2b6d3da284f2e5b209a305
Juan Diego <juand.arevalo221@umariana.edu.co>
2023-04-13 22:49:45 -0500

    RMI con todas las operaciones

--- ServerRMI/src/serverrmi/ServerRMI.java
+++ ServerRMI/src/serverrmi/ServerRMI.java
@@ -4,56 +4,60 @@
  */
 package serverrmi;
 
-import Interfaz.RMIDAO;
-
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import javax.management.remote.rmi.RMIServer;
-
 /**
  *
- * @author juand
+ * @author sistemas
  */
-public class ServerRMI extends UnicastRemoteObject implements RMIDAO{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        try {
-            Registry registro = LocateRegistry.createRegistry(777);
-            registro.rebind("Remoto RMI", new ServerRMI());
-            System.out.println("Seridor esta funcionando");
-        } catch (RemoteException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+import Interface.RMIDAO; //libreria propia
+
+//librerias que se importan
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import javax.management.remote.rmi.RMIServer;
+
+
+public class ServerRMI extends UnicastRemoteObject implements RMIDAO{
 
     public ServerRMI() throws RemoteException{
         super();
     }
 
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try{
+            Registry registro = LocateRegistry.createRegistry(7777);
+            registro.rebind("RemotoRMI", new ServerRMI());
+            System.out.println("Servidor esta activo");
+        }catch(RemoteException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
     @Override
     public int sumar(int numero1, int numero2) throws RemoteException {
-        return numero1+numero2;
+        return numero1 + numero2;
     }
 
     @Override
     public int restar(int numero1, int numero2) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return numero1 - numero2;
     }
 
     @Override
     public int multiplicar(int numero1, int numero2) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return numero1 * numero2;
     }
 
     @Override
-    public float dividir(float numero1, float numero2) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public float dividir(int numero1, int numero2) throws RemoteException {
+        return numero1 / numero2;
     }
 
     @Override
